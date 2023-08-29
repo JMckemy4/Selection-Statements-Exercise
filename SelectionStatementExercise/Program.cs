533b490b224c274984c7323761f0053a79d2607c
jared mckemy <jaredmkcemy4@gmail.com>
2023-08-29 09:27:05 -0700

    selectionstatements

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace SelectionStatementExercise
+﻿using Microsoft.VisualBasic;
+using System.Net.WebSockets;
+
+namespace SelctionStatementExercise
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void buess()
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 10);
+            int guessNbr = 0;
+
+            Console.WriteLine("Try to guess my favorite number");
+            int userInput = int.Parse(Console.ReadLine());
+            if (userInput == favNumber)
+            {
+                Console.WriteLine($"You got it, {favNumber} was it.");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine($"That guess was too high");
+            }
+            else if (userInput < favNumber)
+            {
+                Console.WriteLine($"That guess was too low");
+            }
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
