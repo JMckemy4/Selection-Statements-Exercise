ffceb6cfc7a867d82160d89bbdec7ba5a2cc3b2e
jared mckemy <jaredmkcemy4@gmail.com>
2023-09-19 12:45:32 -0700

    made changes and rewrote with new knowledge

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,29 +1,54 @@
 ﻿using Microsoft.VisualBasic;
 using System.Net.WebSockets;
+using System.Security.Cryptography.X509Certificates;
 
 namespace SelctionStatementExercise
 {
-    public class Program
+    internal class Program
     {
-        static void buess()
+        public static void Main()
         {
+            Console.WriteLine("Try to guess my favorite number");
+            int number = 4;
             var r = new Random();
             var favNumber = r.Next(1, 10);
-            int guessNbr = 0;
 
-            Console.WriteLine("Try to guess my favorite number");
             int userInput = int.Parse(Console.ReadLine());
-            if (userInput == favNumber)
+
+            switch (userInput)
             {
-                Console.WriteLine($"You got it, {favNumber} was it.");
-            }
-            else if (userInput > favNumber)
-            {
-                Console.WriteLine($"That guess was too high");
-            }
-            else if (userInput < favNumber)
-            {
-                Console.WriteLine($"That guess was too low");
+                case int _ when userInput == favNumber:
+                    Console.WriteLine($"You got it, {favNumber} was it.");
+                    break;
+                case int _ when userInput > favNumber:
+                    Console.WriteLine("That guess was too high");
+                    break;
+                case int _ when userInput < favNumber:
+                    Console.WriteLine("That guess was too low");
+                    break;
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
+                    //    Console.WriteLine("Try to guess my favorite number");
+                    //    int number = 4;
+                    //    var r = new Random();
+                    //    var favNumber = r.Next(1, 10);
+                    //    int guessNbr = 0;
+
+                    //    int userInput = int.Parse(Console.ReadLine());
+                    //    if (userInput == favNumber)
+                    //    {
+                    //        Console.WriteLine($"You got it, {favNumber} was it.");
+                    //    }
+                    //    else if (userInput > favNumber)
+                    //    {
+                    //        Console.WriteLine($"That guess was too high");
+                    //    }
+                    //    else if (userInput < favNumber)
+                    //    {
+                    //        Console.WriteLine($"That guess was too low");
+                    //    }
+
             }
         }
     }
